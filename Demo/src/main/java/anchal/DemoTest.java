46d33f8eb4803bd30240e2fe1fe70bbff549db18
Anchal Ajay <40241644+anchal12ajay12@users.noreply.github.com>
2021-04-11 21:52:59 +0530

    Update and rename demoTest.java to DemoTest.java

---
+++ Demo/src/main/java/anchal/DemoTest.java
@@ -0,0 +1,56 @@
+package anchal;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+class Counter{
+
+    private static  Counter single_instance = null;
+    private int count;
+
+    private Counter(){
+    	count = 0;
+    }
+  
+    public static  Counter getInstance(){
+        if (single_instance == null)
+            single_instance = new Counter();
+  
+        return single_instance;
+    }
+    
+    public synchronized void incrementCounter() {
+    	count++;
+    }
+    
+    public int getCount() {
+    	return count;
+    }
+}
+
+public class DemoTest  extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Counter pageHit;
+	
+	public void init(ServletConfig config) throws ServletException {
+		pageHit = Counter.getInstance();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		for(int i=0;i<100000000;i++) pageHit.incrementCounter();
+			out.write("<html><body><h1>Counter: "+ pageHit.getCount()+ "\n</h1></body></html>");
+			
+		
+
+	}
+	
+	public void destroy() {
+		
+	}
+
+}
