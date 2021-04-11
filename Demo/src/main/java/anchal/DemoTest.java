a1f1bea1b36b585e56ef71b8b47ff5026195025c
Anchal Ajay <40241644+anchal12ajay12@users.noreply.github.com>
2021-04-11 21:53:50 +0530

    Update DemoTest.java

--- Demo/src/main/java/anchal/DemoTest.java
+++ Demo/src/main/java/anchal/DemoTest.java
@@ -42,7 +42,7 @@ public class DemoTest  extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
-		for(int i=0;i<100000000;i++) pageHit.incrementCounter();
+		
 			out.write("<html><body><h1>Counter: "+ pageHit.getCount()+ "\n</h1></body></html>");
 			
 		
